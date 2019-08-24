d9077b6e67f7e0ba8c7db69ccabf9ad04f42a371
robert.hao <hypermind@outlook.com>
2019-08-24 13:52:07 +0800

    Add the examples for chapter 03

---
+++ src/ch03/constant/main.jl
@@ -0,0 +1,33 @@
+# 示例的演示文件。
+# - Julia version: 1.1.1
+# - Author: haolin
+# - Date: 2019-07-05
+
+const A = 2020
+println("Constant A: $(A)")
+A = 2050 #= 这里会引发警告。 =#
+println("Constant A: $(A) (redefined)\n")
+
+b = 2070
+println("Variable b: $(b)")
+A = b #= 这里会引发警告。 =#
+println("Constant A: $(A) (redefined)\n")
+
+const C = 2020
+println("Constant C: $(C)")
+f() = C + 30
+println("Invoke function f about C: $(f())")
+C = 2030
+println("Constant C: $(C) (redefined)")
+println("Invoke function f about C: $(f())\n")
+
+# C = "2020"  #= 这里会报错。 =#
+
+const D = "2020"
+println("Constant D: $(D)")
+D = "2020"
+println("Constant D: $(D) (redefined)")
+d = "2020"
+println("Variable d: $(d)")
+D = d
+println("Constant D: $(D) (redefined)")
---
+++ src/ch03/variable/main.jl
@@ -0,0 +1,27 @@
+# 示例的演示文件。
+# - Julia version: 1.1.1
+# - Author: haolin
+# - Date: 2019-07-05
+
+"""
+    get_uint32(x)
+
+把参数`x`的值的类型转换为`UInt32`，并返回转换后的值。
+"""
+function get_uint32(x)
+    # 附加类型标注的变量 y。
+    y::UInt32 = x
+    y
+end
+
+z = get_uint32(2020)
+# 下一行代码中的 z::UInt32 是类型断言。
+println("Invoke function get_uint32 with 2020: $(z::UInt32) (type: $(typeof(z)))")
+
+# 下一行的 x 是全局变量，不能附加类型标注。
+# x::UInt32 = 2020 #= 这里会报错。 =#
+
+# 使用 Ref{T} 类型的全局常量替代全局变量。
+const xref = Ref{UInt32}(2020)
+# 操作符 [] 可用于存/取引用的值。
+println("xref: $(xref[]::UInt32)")
