586ae39a096fb4c139a68f20eb671db0518eb668
robert.hao <hypermind@outlook.com>
2019-09-08 11:43:59 +0800

    Add the examples for chapter 4

---
+++ src/ch04/primitive/main.jl
@@ -0,0 +1,30 @@
+# 示例的演示文件。
+# - Julia version: 1.1.1
+# - Author: haolin
+# - Date: 2019-07-10
+
+primitive type MyUInt64 <: Unsigned 64 end
+
+# 针对 MyUInt64 类型的实例构造方法。
+# 用于根据一个 UInt64 类型的值构造出一个 MyUInt64 类型的值。
+MyUInt64(x::UInt64) = reinterpret(MyUInt64, x)
+
+# 针对 UInt64 类型的实例构造方法。
+# 用于根据一个 MyUInt64 类型的值构造出一个 UInt64 类型的值。
+UInt64(x::MyUInt64) = reinterpret(UInt64, x)
+
+# 显示函数的新方法，针对 MyUInt64 类型。
+Base.show(io :: IO, x :: MyUInt64) = print(io, UInt64(x))
+
+
+"""
+请注意，函数中的表达式`UInt64(666)`和`MyUInt64(x)`表示的都是对构造函数的调用。
+构造函数与对应的类型同名，专用于类型的实例化。
+构造函数也可以拥有衍生方法，以应对不同的构造原材料（如这里的`666`和`x`）。
+比如，我们在前面用简易法定义了一个构造方法`MyUInt64(x::UInt64)`。
+这个构造方法可以根据一个`UInt64`类型的值，构造出一个`MyUInt64`类型的实例。
+正因为如此，下面的函数调用`MyUInt64(x)`才能够成功。
+"""
+x = UInt64(666)
+y = MyUInt64(x)
+println(UInt64(y))
---
+++ src/ch04/structure/main.jl
@@ -0,0 +1,31 @@
+# 示例的演示文件。
+# - Julia version: 1.1.1
+# - Author: haolin
+# - Date: 2019-07-10
+
+# 代表用户的不可变复合类型。
+struct User
+    name::String
+    reg_year::UInt16
+    extra
+end
+
+# 代表个人的可变复合类型。
+mutable struct Person
+    name::String
+    age::UInt8
+    extra
+end
+
+u1 = User("Robert", 2000, "something")
+u2 = User("Robert", UInt16(2000), "something")
+println("u1: $(u1)")
+println("u2: $(u2)")
+println("u1 === u2? $(u1 === u2)")
+
+p1 = Person("Robert", 30, "something")
+p1.age = 37
+p2 = Person("Robert", 37, "something")
+println("p1: $(p1)")
+println("p2: $(p2)")
+println("p1 === p2? $(p1 === p2)")
